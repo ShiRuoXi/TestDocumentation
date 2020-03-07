227a54af10260efb72a992ca92e586ed7d5d29f2
石小白 <652390281@qq.com>
2020-03-07 17:45:55 +0800

    创建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+//https://www.thingjs.com/s/123f5119a292bbf38a213ff6
+module.exports = ctx => ({
+    title: '约定文档', 
+  
+    themeConfig: {
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated', 
+        nav: [
+            { text: '项目演示地址', link: '' },
+            { text: '关于', link: '' },
+        ],
+        sidebar: [
+            ['/', '简介'],
+            ['/dome/home.md', '接口类型说明']
+        ]
+    }
+})
